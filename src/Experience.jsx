4ccec5b9ecb28098d5f3abe52218aa6a2e063605
chimakes <chimakes3d@gmail.com>
2026-04-04 14:02:15 -0400

    Add stencil to materials

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF, Environment } from "@react-three/drei"
+import { Mask, OrbitControls, Environment } from "@react-three/drei"
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Bird from './Bird.jsx'
@@ -6,19 +6,45 @@ import Bird from './Bird.jsx'
 export default function Experience()
 {
     return <>
-        <Canvas>
+        <Canvas gl={{ stencil: true }}>
             {/* lighting */}
             <directionalLight position={[1, 2, 1.5]} intensity={2.5}  />
             <hemisphereLight args={["#fff7e6", "#b8c1ff", 2.0]} />
 
-            <OrbitControls />
+            <OrbitControls makeDefault />
 
             <Suspense fallback={null}>
                 <Environment preset="city" />
 
-                <Bird scale={2} position={[0, -0.5, 0]} />
+                <CircularMask />
+
+                <Bird scale={1.8} rotation-x={Math.PI * 0.03} position-x={1}/>
             </Suspense>
         </Canvas>
     </>
 }
+
+const CircularMask = (props) => (
+  <group {...props}>
+      <Frame position={[0, 0, 1]} />
+      <Mask id={1} position={[0, 0, 0.95]}>
+        <circleGeometry args={[0.8, 64]} />
+      </Mask>
+  </group>
+)
+
+const Frame = (props) => (
+  <mesh {...props}>
+    <ringGeometry args={[0.785, 0.85, 64]} />
+    <meshPhongMaterial color="black" />
+  </mesh>
+)
+
+// const Box = ({ args = [1, 4, 1], radius = 0.1, smoothness = 4, bevelSegments=4, color = 'black', ...boxProps }) => (
+//   <RoundedBox args={args} radius={radius} smoothness={smoothness} bevelSegments={bevelSegments} {...boxProps}>
+//     <meshPhongMaterial color={color} />
+//   </RoundedBox>
+// )
+
+
 
